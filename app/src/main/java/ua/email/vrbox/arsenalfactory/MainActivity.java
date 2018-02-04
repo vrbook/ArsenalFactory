00f8a21f04d432f01a2dadb4d197319e0264b5db
vrbdsp <vrbbox@yahoo.com>
2018-02-04 12:45:09 +0200

    Authorysation

--- app/src/main/java/ua/email/vrbox/arsenalfactory/MainActivity.java
+++ app/src/main/java/ua/email/vrbox/arsenalfactory/MainActivity.java
@@ -1,15 +1,31 @@
 package ua.email.vrbox.arsenalfactory;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button butSign;
+
+    String uid;
+
+    private EditText textEmail;
+    private EditText textPassword;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +34,74 @@ public class MainActivity extends AppCompatActivity {
 
         butSign = (Button) findViewById(R.id.button2);
         butSign.setOnClickListener(onClickListener);
+
+        textEmail = (EditText) findViewById(R.id.editEmail);
+        textPassword = (EditText) findViewById(R.id.editPass);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            Toast.makeText(MainActivity.this, "Авторизация", Toast.LENGTH_SHORT).show();
-
-            Intent intent = new Intent(getApplicationContext(), ViewActivity.class);
-            startActivity(intent);
-
+            signing(textEmail.getText().toString(),textPassword.getText().toString());
         }
     };
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private FirebaseAuth.AuthStateListener mAuthListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user != null) {
+                // User is signed in
+                uid = user.getUid();
+                Toast.makeText(MainActivity.this, "Авторизация успешна. " + uid, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), ViewActivity.class);
+                startActivity(intent);
+
+
+            } else {
+                // User is signed out
+
+                Toast.makeText(MainActivity.this, "Авторизация не успешна.", Toast.LENGTH_SHORT).show();
+
+            }
+            // ...
+        }
+    };
+
+
+
+
+    public void signing(String email, String password){
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Авторизация успешна.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Авторизация не выполнена.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
 }
